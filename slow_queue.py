398ea2f17a58a778413ead3e36ea025f85c0f36a
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2023-03-03 14:20:29 +0100

    Fixed prefetch setting

--- slow_queue.py
+++ slow_queue.py
@@ -1,6 +1,6 @@
 import pika
-# from objection_engine.v4.make_movie import render_comment_list
-from objection_engine.renderer import render_comment_list
+from objection_engine.v4.make_movie import render_comment_list
+# from objection_engine.renderer import render_comment_list
 from objection_engine.beans.comment import Comment
 from lib import callback
 
@@ -18,10 +18,10 @@ def callback_interceptor(ch, method, props, body):
 
 connection = pika.BlockingConnection()
 channel = connection.channel()
+channel.basic_qos(prefetch_count=1)
 
 channel.queue_declare(queue='oe_slow')
 
 channel.basic_consume(queue='oe_slow', on_message_callback=callback_interceptor)
 print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.basic_qos(prefetch_count=1)
 channel.start_consuming()
